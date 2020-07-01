48e0406dc91e25fab4c4a4416698c9022eca94c8
gustavo <pereiragustavo215@gmail.com>
2020-07-01 16:04:04 -0300

    inputTypePersonalizado

--- app/src/main/java/com/cursomaterialdesign/fragment/InputTypePersonalizadoFragment.kt
+++ app/src/main/java/com/cursomaterialdesign/fragment/InputTypePersonalizadoFragment.kt
@@ -1,10 +1,11 @@
-package com.cursomaterialdesign
+package com.cursomaterialdesign.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.cursomaterialdesign.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [BlankFragment.newInstance] factory method to
+ * Use the [InputTypePersonalizadoFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BlankFragment : Fragment() {
+class InputTypePersonalizadoFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class BlankFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        return inflater.inflate(R.layout.fragment_input_type_personalizado, container, false)
     }
 
     companion object {
@@ -49,7 +50,7 @@ class BlankFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            BlankFragment().apply {
+            InputTypePersonalizadoFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/cursomaterialdesign/main/MainActivity.kt
+++ app/src/main/java/com/cursomaterialdesign/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.cursomaterialdesign
+package com.cursomaterialdesign.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cursomaterialdesign.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
